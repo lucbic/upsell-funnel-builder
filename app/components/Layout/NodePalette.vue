bc7fdd47b78136d3488af146ec08a45f01a244a4
Lucas Bicudo <lucbic@gmail.com>
2026-02-05 15:26:54 -0300

    refactor: use FunnelNode component in NodePalette for consistent styling

--- app/components/Layout/NodePalette.vue
+++ app/components/Layout/NodePalette.vue
@@ -19,7 +19,7 @@
 
 <template>
   <div class="flex flex-col gap-2 p-4">
-    <h3 class="mb-2 text-sm font-semibold uppercase">
+    <h3 class="mb-2 text-xs font-semibold uppercase">
       Funnel Steps
     </h3>
 
@@ -27,21 +27,18 @@
       v-for="[type, config] in nodeTypes"
       :key="type"
       draggable="true"
-      class="flex cursor-grab items-center gap-3 rounded-lg
-        border border-gray-700 bg-gray-900 px-3 py-2.5
-        transition-all hover:border-gray-500
-        hover:bg-gray-800 active:cursor-grabbing"
+      class="cursor-grab transition-all
+        active:cursor-grabbing"
       @dragstart="onDragStart($event, type)"
     >
-      <UIcon
-        :name="config.icon"
-        :class="getNodeIconClass(type)"
-        class="size-4"
+      <FunnelNode
+        palette
+        :data="{
+          nodeType: type,
+          icon: config.icon,
+          title: config.label
+        }"
       />
-
-      <span class="text-sm text-white">{{
-        config.label
-      }}</span>
     </div>
   </div>
 </template>
