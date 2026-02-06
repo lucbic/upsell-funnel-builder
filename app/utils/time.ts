177df872689f2611df82078dab26d134c498102f
Lucas Bicudo <lucbic@gmail.com>
2026-02-06 19:13:05 -0300

    refactor(time): use useTimeoutFn from vueuse instead of raw setTimeout

--- app/utils/time.ts
+++ app/utils/time.ts
@@ -1,3 +1,4 @@
-export const wait = (ms: number) => {
-  return new Promise(resolve => setTimeout(resolve, ms))
-}
+export const wait = (ms: number) =>
+  new Promise<void>(resolve => {
+    useTimeoutFn(resolve, ms)
+  })
