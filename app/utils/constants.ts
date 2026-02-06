7962b42223abdea7853161cb05f79a1d81fe1ba7
Lucas Bicudo <lucbic@gmail.com>
2026-02-06 19:13:10 -0300

    refactor(constants): add timing constants and grid spacing

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -1,5 +1,12 @@
-export const useConstants = () => ({
-  NODE_WIDTH: 200,
-  NODE_HEIGHT: 75,
-  KEYBOARD_INSERT_NODE_GAP: 75
-})
+export const useConstants = () =>
+  ({
+    GRID_SPACING: 25,
+    NODE_WIDTH: 200,
+    NODE_HEIGHT: 75,
+    KEYBOARD_INSERT_NODE_GAP: 75,
+    DURATION: {
+      FIT_VIEW: 300,
+      LOADING_DELAY: 350,
+      ZOOM_TRANSITION: 100
+    }
+  }) as const
